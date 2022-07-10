705dcd2cdf11dd8f9091f32a20336d3b9248eed5
legyllith <jeremy.n.49@live.fr>
2022-07-10 19:45:49 +0200

    connection bdd cote serveur

--- io/conf.js
+++ io/conf.js
@@ -35,3 +35,26 @@ setInterval(update, 1000/60);
 
 };
 
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+     host: 'localhost', 
+     user:'root', 
+     password: 'ludiMON1996',
+     connectionLimit: 5
+});
+async function asyncFunction() {
+  let conn;
+  try {
+	conn = await pool.getConnection();
+	const rows = await conn.query("SELECT 1 as val");
+	console.log(rows); //[ {val: 1}, meta: ... ]
+	//const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
+	//console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+
+  } catch (err) {
+	throw err;
+  } finally {
+	if (conn) return conn.end();
+  }
+}
+
