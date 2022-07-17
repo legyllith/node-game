21878ab8815ddd12e9fd8d1d3223c59b391a627e
legyllith <jeremy.n.49@live.fr>
2022-07-17 23:50:10 +0200

    le app.js est dans le fichier de base

--- app.js
+++ app.js
@@ -6,3 +6,14 @@ var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 module.exports = app;
+
+
+// Middleware
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+})
+app.get('/login', (req, res) => {
+    res.render('../login')
+})
