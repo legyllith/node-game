b3ceb8b22556c17321df2bc88eb3637a9c32106b
legyllith <jeremy.n.49@live.fr>
2022-07-21 08:57:42 +0200

    unityserver

---
+++ unityServer/server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+
+var app = express();
+
+app.get("/user/:id", (req, res) => {
+
+	var dummyData = {
+		"userid": req.params["id"],
+		"username": "quill" + req.params["id"],
+		"wins": 18,
+		"lossing": 1000,
+	};
+
+	red.send(dummyData.username);
+});
+
+app.listen(3000, () => {
+	console.log("Server has started");
+});
