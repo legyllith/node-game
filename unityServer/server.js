9e7ca96cdced547dc5205d2de7dab38cc1cd966e
legyllith <jeremy.n.49@live.fr>
2022-07-22 20:37:11 +0200

    add new

--- unityServer/server.js
+++ unityServer/server.js
@@ -49,7 +49,7 @@ io.on('connection', (socket) => {
     });
     socket.on('disconnect', () => {
         console.log('user disconnected');
-        io.emit('deconnected player', Object.values(socket.id));
+        io.emit('deconnected player', Object.value(socket.id));
       	delete players[socket.id];
     });
 
