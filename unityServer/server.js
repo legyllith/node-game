ad874ea7b2f9ccac7082ce55c4620c482b6960a2
legyllith <jeremy.n.49@live.fr>
2022-07-22 10:09:01 +0200

    Update server.js

--- unityServer/server.js
+++ unityServer/server.js
@@ -19,6 +19,24 @@ app.get("/user/:id", (req, res) => {
 	res.send(dummyData);
 });
 
-app.listen(3000, () => {
-	console.log("Server has started");
+//app.listen(3000, () => {
+//	console.log("Server has started");
+//});
+
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+http.listen(3000, () => {
+    console.log('Connected at 3000');
 });
